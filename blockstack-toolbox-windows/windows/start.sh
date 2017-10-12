2d1e18650416b99d1d502c2e1710cfb229775f24
Aaron Blankstein <aaron@blockstack.com>
2017-10-12 10:41:47 -0400

    add port information and instructions to connect to the browser to the startup screen

--- blockstack-toolbox-windows/windows/start.sh
+++ blockstack-toolbox-windows/windows/start.sh
@@ -124,6 +124,11 @@ launcher start
 trap "launcher stop ; echo 'Closing'" SIGINT SIGTERM
 trap "launcher stop ; echo 'Closing'" EXIT
 
+echo "Blockstack's background processes are listening on ports 6270 and 1337"
+echo
+echo "The Blockstack browser portal is listening on port 8888"
+echo "To connect to the Blockstack browser, open http://localhost:8888 in your browser"
+echo
 echo "Blockstack is running. Hit Ctrl-C to Exit."
 
 while :
